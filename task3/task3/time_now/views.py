bf4b105c25c2e3c4d999f67f80866fc25c2f3786
Artem Khymenko <breakerhim@ua.fm>
2017-05-30 15:39:59 +0300

    Add static css

--- task3/task3/time_now/views.py
+++ task3/task3/time_now/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def show_time(request):
-    return render(request, 'main.html')
+    now = datetime.datetime.now()
+    return render(request, 'main.html', {'now':now})
