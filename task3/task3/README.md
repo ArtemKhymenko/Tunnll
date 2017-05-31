60b81ab00f1ac25ed6766b4fd17218c25b744d41
Artem Khymenko <breakerhim@ua.fm>
2017-05-31 16:48:08 +0300

    Create Dockerfile, requirements.txt and README

---
+++ task3/task3/README.md
@@ -0,0 +1,1 @@
+Task 3. Adaptive landing page with Django
--- task3/task3/time_now/urls.py
+++ task3/task3/time_now/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from views import show_time
+from .views import show_time
 
 urlpatterns = [
     url(r'^$', show_time),
