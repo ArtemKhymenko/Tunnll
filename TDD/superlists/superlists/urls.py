516a06294c8a57031b437529ceb7a4ca268deab8
Artem Khymenko <breakerhim@ua.fm>
2017-06-06 22:28:03 +0300

    Change urls

--- TDD/superlists/superlists/urls.py
+++ TDD/superlists/superlists/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lists.views import home_page, view_list, new_list, add_item
+from lists import views
 
 urlpatterns = [
-    url(r'^$', home_page, name='home'),
-    url(r'^lists/(\d+)/$', view_list, name='view_list'),
-    url(r'lists/(\d+)/add_item$', add_item, name='add_item'),
-    url(r'^lists/new$', new_list, name='new_list')
-
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/', include('lists.urls')),
 ]
